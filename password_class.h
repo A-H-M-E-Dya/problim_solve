e5d1f25a3b3e6318f1be1e1bbe10f05011028c08
A-H-M-E-Dya <74510393+A-H-M-E-Dya@users.noreply.github.com>
2022-03-26 18:59:04 +0200

    Add files via upload

---
+++ password_class.h
@@ -0,0 +1,9 @@
+using namespace std;
+class Pass{
+    private:
+    string password;
+    public:
+    void set_password(string pass);
+    string get_password();
+
+};
---
+++ password_strac.h
@@ -0,0 +1,8 @@
+#include "password_class.h"
+
+void Pass::set_password(string pass){
+    password = pass;
+}
+string Pass::get_password(){
+    return password;
+}
